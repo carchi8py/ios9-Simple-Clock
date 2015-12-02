ca415a47fc40257c88bcb3f178f651b1389cd478
Chris Archibald <carchi8py@gmail.com>
2015-12-01 19:20:52 -0800

    changes

--- Simple Clock/ViewController.swift
+++ Simple Clock/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timeLabel.text = ""
+        let timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("updateClock"), userInfo: nil, repeats: true)
+    }
+    
+    func updateClock() {
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Hour, .Minute, .Second], fromDate: date)
+        
+        // Hour is returned in miliary time
+        var hour = components.hour > 12 ? components.hour - 12 : components.hour
+        hour = hour == 0 ? 12 : hour
+        
+        //Adds a 0 to the front of the hour if it less than 9
+        let hourString = appendZero(hour)
+        let minuteString = appendZero(components.minute)
+        let secondString = appendZero(components.second)
+        
+        let am = components.hour > 12 ? "PM" : "AM"
+        
+        timeLabel.text = "\(hourString):\(minuteString):\(secondString) \(am)"
+        
+    }
+    
+    func appendZero(time:Int) -> String
+    {
+        return time > 9 ? "\(time)" : "0\(time)"
     }
 
     override func didReceiveMemoryWarning() {
